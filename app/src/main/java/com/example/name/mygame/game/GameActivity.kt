30630d6a370da08ffe535962580513584811f5bb
rail-rain <rail91403@gmail.com>
2017-06-12 12:10:54 +0900

    rename and refactor

--- app/src/main/java/com/example/name/mygame/game/GameActivity.kt
+++ app/src/main/java/com/example/name/mygame/game/GameActivity.kt
@@ -1,22 +1,18 @@
 package com.example.name.mygame.game
 
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
-import android.support.v7.app.AppCompatActivity
-import android.view.View
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 
-class Main : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
 
     private val game_loop = Executors.newSingleThreadScheduledExecutor()
     private var game_loop_future: ScheduledFuture<*>? = null
     private lateinit var view: View
-    private val loop_handler = object: Handler() {
-        override fun handleMessage(msg: Message) = view.invalidate()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +25,8 @@ class Main : AppCompatActivity() {
         super.onResume()
         game_loop_future = game_loop.scheduleAtFixedRate({
             Reducer.update()
-            loop_handler.sendMessage(Message.obtain())
-        }, 0, 33, TimeUnit.MICROSECONDS)
+            view.postInvalidate()
+        }, 0, 33, TimeUnit.MILLISECONDS)
     }
 
     override fun onPause() {
