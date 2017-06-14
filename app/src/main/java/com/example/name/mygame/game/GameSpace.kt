8046f460b4f1643b050eef97bd853e6320a6e61f
rail-rain <rail91403@gmail.com>
2017-06-14 19:17:04 +0900

    fix grund

--- app/src/main/java/com/example/name/mygame/game/GameSpace.kt
+++ app/src/main/java/com/example/name/mygame/game/GameSpace.kt
@@ -11,7 +11,7 @@ import java.util.*
 object GameSpace {
     private const val MAX_PIECE = 45
     private const val PIECE_SIZE= 1f
-    private const val SPACE_WIDTH = 9.47f
+    private const val SPACE_WIDTH = 8.5f
     private const val SPACE_HEIGHT = 10f
 
     private val world = World(Vec2(0f, 9.8f))
@@ -76,9 +76,9 @@ object GameSpace {
         val shape = ChainShape()
         shape.createChain(arrayOf(
                 Vec2(0f, 0f),
-                Vec2(0f, SPACE_HEIGHT - (SPACE_WIDTH / 3)),
+                Vec2(0f, SPACE_HEIGHT * 0.9f),
                 Vec2(SPACE_WIDTH / 2, SPACE_HEIGHT),
-                Vec2(SPACE_WIDTH, SPACE_HEIGHT - (SPACE_WIDTH / 3)),
+                Vec2(SPACE_WIDTH, SPACE_HEIGHT * 0.9f),
                 Vec2(SPACE_WIDTH, 0f)
         ), 5)
 
