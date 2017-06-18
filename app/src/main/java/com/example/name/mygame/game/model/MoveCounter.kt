ef76abe137926b2d16d255244209fb2ea358a158
rail-rain <rail91403@gmail.com>
2017-06-18 11:40:16 +0900

    add MoveCounter

---
+++ app/src/main/java/com/example/name/mygame/game/model/MoveCounter.kt
@@ -0,0 +1,10 @@
+package com.example.name.mygame.game.model
+
+class MoveCounter {
+    var count: Int = 10
+        private set
+    fun move() {
+        count--
+    }
+    fun isEnd() = count != 0
+}
