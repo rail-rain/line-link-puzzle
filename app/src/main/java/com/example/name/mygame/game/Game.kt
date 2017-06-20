ba99307380415ff3b53e3d61cd2b029d16d4255b
rail-rain <rail91403@gmail.com>
2017-06-20 10:36:05 +0900

    mainView to abstract

--- app/src/main/java/com/example/name/mygame/game/Game.kt
+++ app/src/main/java/com/example/name/mygame/game/Game.kt
@@ -3,24 +3,30 @@ package com.example.name.mygame.game
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import com.example.name.mygame.R
 import com.example.name.mygame.game.model.GameSystem
+import com.example.name.mygame.game.ui.GameUI
 import com.example.name.mygame.game.util.GameActivity
 import com.example.name.mygame.game.ui.MainView
 import com.example.name.mygame.game.ui.InputProcessor
 import com.example.name.mygame.game.viewmodel.Driver
+import org.jetbrains.anko.find
+import org.jetbrains.anko.setContentView
 
 class Game : GameActivity<Transition>(fps = 30) {
     companion object {
         const val SCORE = "s"
     }
     override val system: GameSystem = GameSystem()
+    override val mainView by lazy { find<View>(R.id.main_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val driver = Driver(this, system)
-        mainView = MainView(this, driver)
-        val inputProcessor = InputProcessor(driver)
-        mainView.setOnTouchListener(inputProcessor)
+        GameUI(driver).setContentView(this)
+        InputProcessor(this, driver)
     }
 
     override fun transit(transition: Transition) {
