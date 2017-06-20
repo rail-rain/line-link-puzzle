c9dd8f39e461d6a9ee25dbb9a214263775c933d1
rail-rain <rail91403@gmail.com>
2017-06-20 11:13:37 +0900

    GameActivity add pause menu support

--- app/src/main/java/com/example/name/mygame/game/util/GameActivity.kt
+++ app/src/main/java/com/example/name/mygame/game/util/GameActivity.kt
@@ -1,6 +1,8 @@
 package com.example.name.mygame.game.util
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
 import android.view.View
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledFuture
@@ -12,6 +14,7 @@ abstract class GameActivity<T>(fps: Long): AppCompatActivity() {
     private val period = 1000 / fps
 
     protected abstract val mainView: View
+    protected abstract val pauseMenu: View
     protected abstract val system: System<T>
 
     protected abstract fun transit(transition: T)
@@ -26,24 +29,34 @@ abstract class GameActivity<T>(fps: Long): AppCompatActivity() {
 
     fun resume() {
         future = executor.scheduleAtFixedRate({ this.update() }, 0, period, TimeUnit.MILLISECONDS)
+        pauseMenu.visibility = View.INVISIBLE
     }
 
     fun pause() {
         future!!.cancel(true)
+        pauseMenu.visibility = View.VISIBLE
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
         resume()
     }
 
     override fun onPause() {
         super.onPause()
-        pause()
+        if (pauseMenu.visibility == View.INVISIBLE) pause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         executor.shutdown()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            pause()
+            return false
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
