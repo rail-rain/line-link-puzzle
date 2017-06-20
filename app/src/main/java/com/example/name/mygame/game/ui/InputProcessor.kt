7cf56be2e53f934d58ec74f170b2ebbd9e129e5b
rail-rain <rail91403@gmail.com>
2017-06-20 12:59:03 +0900

    add resume and quit action

--- app/src/main/java/com/example/name/mygame/game/ui/InputProcessor.kt
+++ app/src/main/java/com/example/name/mygame/game/ui/InputProcessor.kt
@@ -1,16 +1,33 @@
 package com.example.name.mygame.game.ui
 
+import android.app.Activity
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Button
+import com.example.name.mygame.R
 import com.example.name.mygame.game.viewmodel.Actions
+import org.jetbrains.anko.find
 
+class InputProcessor(activity: Activity, private val dispatcher: Actions): View.OnTouchListener, View.OnClickListener {
 
-class InputProcessor(private val dispatcher: Actions): View.OnTouchListener {
+    init {
+        activity.find<Button>(R.id.resume).setOnClickListener(this)
+        activity.find<Button>(R.id.quit).setOnClickListener(this)
+        activity.find<View>(R.id.main_view).setOnTouchListener(this)
+    }
+
     override fun onTouch(v: View, event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_MOVE -> dispatcher.selectPiece(event.x, event.y)
             MotionEvent.ACTION_UP -> dispatcher.endSelectingPieces()
         }
         return true
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.resume -> dispatcher.resume()
+            R.id.quit -> dispatcher.quit()
+        }
     }
 }
--- app/src/main/java/com/example/name/mygame/game/viewmodel/Actions.kt
+++ app/src/main/java/com/example/name/mygame/game/viewmodel/Actions.kt
@@ -3,4 +3,6 @@ package com.example.name.mygame.game.viewmodel
 interface Actions {
     fun selectPiece(selectedX: Float, selectedY: Float)
     fun endSelectingPieces()
+    fun resume()
+    fun quit()
 }
