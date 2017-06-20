1818bbb890ba1b3839314f889778b639c14df471
rail-rain <rail91403@gmail.com>
2017-06-20 12:38:21 +0900

    update System and open class

--- app/src/main/java/com/example/name/mygame/game/model/GameSystem.kt
+++ app/src/main/java/com/example/name/mygame/game/model/GameSystem.kt
@@ -1,34 +1,27 @@
 package com.example.name.mygame.game.model
 
-import com.example.name.mygame.game.Transition
+import com.example.name.mygame.game.util.GameActivity
 import com.example.name.mygame.game.util.System
 
-class GameSystem: System<Transition> {
-    private val score = Score()
-    private val gameSpace = GameSpace()
-    private val counter = MoveCounter()
+open class GameSystem(gameActivity: GameActivity): System(gameActivity) {
+    protected val score = Score()
+    protected val gameSpace = GameSpace()
+    protected val counter = MoveCounter()
 
-    override var transition: Transition? = null
-
-    fun selectPiece(selectedX: Float, selectedY: Float) {
+    open fun selectPiece(selectedX: Float, selectedY: Float) {
         gameSpace.selectPiece(selectedX, selectedY)
     }
 
-    fun endSelectingPieces() {
+    open fun endSelectingPieces() {
         val size = gameSpace.selectedPieces.size
         if (gameSpace.endSelectingPieces()) {
             score.increaseScore(size)
             counter.move()
-            if (counter.isEnd()) transition = Transition.End(getScore())
+            if (counter.isEnd()) end(score.current)
         }
     }
 
     override fun update() {
         gameSpace.step()
     }
-
-    fun getScore() = score.current
-    fun getCount() = counter.count
-    fun getPieces() = gameSpace.pieces
-    fun getSelectedPieces() = gameSpace.selectedPieces
 }
